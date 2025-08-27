54bfc82a62daae0dec67a3302220962de336411f
Rickoroxd <0954744487zaz@gmail.com>
2025-08-28 00:17:22 +0700

    Make New

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
 import socket
-import asyncio
-import concurrent.futures
 
- 
 
-def port_scanner(host:str,port: int) -> bool:
+
+def port_scanner(host : str,pott : int) -> bool:
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    s.settimeout(timeout = 0.5)
-    return s.connect_ex((host,port)) == 0
-
-
-port_num = [80,443,22]
-
-
-
+    
+    try:
 
