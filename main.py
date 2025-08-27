5b1d943c5db1ac97f4f9a43de91ae75a630cce20
Rickoroxd <0954744487zaz@gmail.com>
2025-08-28 00:24:26 +0700

    better

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import socket
 
 
 
-def port_scanner(host : str,pott : int) -> bool:
+def port_scanner(host : str,port : int) -> bool:
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    
     try:
+        s.connect_ex((host,port))
+        print(f'{host} : OPENED')
+        s.close()
+    except:
+        print('f{host} : CLOSED')
+
 
