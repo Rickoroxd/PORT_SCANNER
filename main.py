56f4b7a9c813297a5443bfa0461465648ebbf6c0
Rickoroxd <0954744487zaz@gmail.com>
2025-08-28 20:17:18 +0700

    add more line

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import socket
 
-
-
 def port_scanner(host : str,port : int) -> bool:
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     try:
-        s.connect_ex((host,port))
-        print(f'{host} : OPENED')
-        s.close()
-    except:
-        print('f{host} : CLOSED')
+        if(s.connect_ex((host,port)) == 0):
+            print(f'HOST : {host} PORT : {port} is OPENED')
+        else :
+             print(f'HOST : {host} PORT : {port} is CLOSED')
+        s.close() 
+    except KeyboardInterrupt:
+        print("Something Wrong with Input")
+
+
+port = [22,23,80,443]
+host = "scanme.nmap.org"
+
+for i in port:
+    port_scanner(host,i)
 
 
